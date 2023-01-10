9a8695139e380070c049c2a0b593390e2294375f
Demian Odasso <demianodasso@Demians-MacBook-Pro.local>
2023-01-10 00:20:29 -0300

    Use DI to inject a course repository

--- lib/ui/courses/courses_controller.dart
+++ lib/ui/courses/courses_controller.dart
@@ -1,9 +1,10 @@
 import 'package:rw_courses/model/course.dart';
 import 'package:rw_courses/repository/repository.dart';
-import 'package:rw_courses/repository/repository_impl.dart';
 
 class CoursesController {
-  final Repository _repository = RepositoryImpl();
+  final Repository _repository;
+
+  CoursesController(this._repository);
 
   Future<List<Course>> fetchCourses(int? domainFilter) {
     return _repository.getCourses(domainFilter);
--- lib/ui/courses/courses_page.dart
+++ lib/ui/courses/courses_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rw_courses/repository/repository_impl.dart';
 import 'package:rw_courses/ui/courses/courses_controller.dart';
 
 class CoursesPage extends StatefulWidget {
@@ -9,7 +10,7 @@ class CoursesPage extends StatefulWidget {
 }
 
 class _CoursesPageState extends State<CoursesPage> {
-  final _controller = CoursesController();
+  final _controller = CoursesController(RepositoryImpl());
 
   @override
   Widget build(BuildContext context) {
