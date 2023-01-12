87aa6449640bcd7997b302e4a762f301f5a5c5bf
Demian Odasso <demianodasso@Demians-MacBook-Pro.local>
2023-01-11 23:04:54 -0300

    Sets orientation to portrait

--- lib/rw_courses_app.dart
+++ lib/rw_courses_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rw_courses/strings.dart';
 import 'package:rw_courses/ui/courses/courses_page.dart';
 import 'package:rw_courses/ui/filter/filter_page.dart';
@@ -8,6 +9,7 @@ class RwCoursesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return Scaffold(
       appBar: AppBar(
         title: const Text(Strings.appTitle),
